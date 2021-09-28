16faa3173b273c1eeb36305b0c6836f330062ad9
Sammy Mutahi <sammymutahi12@gmail.com>
2021-09-28 12:23:24 +0300

    add recyclerview in activity_main.xml

--- app/src/main/java/com/sammy/solidprinciples/MainActivity.kt
+++ app/src/main/java/com/sammy/solidprinciples/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sammy.solidprinciples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sammy.solidprinciples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
     }
 }
